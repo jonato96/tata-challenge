c7c48a8482c1251c49859d9b70755ab081059ee6
jonato96 <jonato96s@gmail.com>
2024-06-25 20:46:39 -0500

    break-into-base-microservices

---
+++ ms-client/src/test/java/com/tcs/client/MsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
